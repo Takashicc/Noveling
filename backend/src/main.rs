e82734b5f7f409a0536e4c7226bcb38e402b2043
Takashicc <105186894+Takashicc@users.noreply.github.com>
2022-12-24 17:15:23 +0900

    feat: Run example server

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+mod api;
+mod models;
+mod repository;
+
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello World!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(index))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
