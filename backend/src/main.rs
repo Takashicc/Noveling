58fdd9b97da8974d8dff8bb738ca1657a155b9a4
Takashicc <105186894+Takashicc@users.noreply.github.com>
2022-12-28 16:32:25 +0900

    feat: Set database to app data

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -2,7 +2,9 @@ mod api;
 mod models;
 mod repository;
 
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, web::Data, App, HttpResponse, HttpServer, Responder};
+// use api::user_api::{create_user, delete_user, get_user, update_user};
+use repository::mongodb_repos::MongoRepo;
 
 #[get("/")]
 async fn index() -> impl Responder {
@@ -11,8 +13,17 @@ async fn index() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(index))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    let db = MongoRepo::init().await;
+    let db_data = Data::new(db);
+
+    HttpServer::new(move || {
+        App::new().app_data(db_data.clone()).service(index)
+        // .service(create_user)
+        // .service(get_user)
+        // .service(update_user)
+        // .service(delete_user)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
