6411ecceea9885bab4ebeebd8fbc438fb0a11f2b
Takashicc <105186894+Takashicc@users.noreply.github.com>
2022-12-30 20:43:14 +0900

    feat: Add user_exists_by_email, create_user

--- backend/src/repository/mongodb_repos.rs
+++ backend/src/repository/mongodb_repos.rs
@@ -1,14 +1,10 @@
 use crate::models::user_model::User;
 use dotenv::dotenv;
-use mongodb::{
-    bson::{doc, extjson::de::Error, oid::ObjectId},
-    results::{DeleteResult, InsertOneResult, UpdateResult},
-    Client, Collection,
-};
+use mongodb::{bson::doc, Client, Collection};
 use std::env;
 
 pub struct MongoRepo {
-    col: Collection<User>,
+    pub user_col: Collection<User>,
 }
 
 impl MongoRepo {
@@ -25,34 +21,8 @@ impl MongoRepo {
 
         let col: Collection<User> = db.collection("User");
 
-        MongoRepo { col }
+        MongoRepo { user_col: col }
     }
-
-    // pub async fn create_user(&self, new_user: User) -> Result<InsertOneResult, Error> {
-    //     let doc = User {
-    //         id: None,
-    //         name: new_user.name,
-    //     };
-    //     let user = self
-    //         .col
-    //         .insert_one(doc, None)
-    //         .await
-    //         .expect("Error creating user");
-
-    //     Ok(user)
-    // }
-
-    // pub async fn get_user(&self, id: &String) -> Result<User, Error> {
-    //     let obj_id = ObjectId::parse_str(id).unwrap();
-    //     let filter = doc! {"_id": obj_id};
-    //     let user = self
-    //         .col
-    //         .find_one(filter, None)
-    //         .await
-    //         .expect("Error getting user's detail");
-
-    //     Ok(user.unwrap())
-    // }
 
     // pub async fn update_user(&self, id: &String, new_user: User) -> Result<UpdateResult, Error> {
     //     let obj_id = ObjectId::parse_str(id).unwrap();
