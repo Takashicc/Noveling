74e5c420471e2102122d6ef063904b9ae14d1a2e
Takashicc <105186894+Takashicc@users.noreply.github.com>
2022-12-30 20:45:13 +0900

    feat: Add api_config

---
+++ backend/src/config/api_config.rs
@@ -0,0 +1,7 @@
+use actix_web::web::{self, ServiceConfig};
+
+use crate::api::auth_api;
+
+pub fn config_services(config: &mut ServiceConfig) {
+    config.service(web::scope("/api").service(web::scope("/auth").service(auth_api::signup)));
+}
