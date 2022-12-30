99297dcf79e413edf4074076ab7ea209db71d798
Takashicc <105186894+Takashicc@users.noreply.github.com>
2022-12-30 20:46:26 +0900

    feat: Set the routes by configure method

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,15 +1,11 @@
 mod api;
+mod config;
+mod errors;
 mod models;
 mod repository;
 
-use actix_web::{get, web::Data, App, HttpResponse, HttpServer, Responder};
-// use api::user_api::{create_user, delete_user, get_user, update_user};
+use actix_web::{web::Data, App, HttpServer};
 use repository::mongodb_repos::MongoRepo;
-
-#[get("/")]
-async fn index() -> impl Responder {
-    HttpResponse::Ok().body("Hello World!")
-}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -17,11 +13,9 @@ async fn main() -> std::io::Result<()> {
     let db_data = Data::new(db);
 
     HttpServer::new(move || {
-        App::new().app_data(db_data.clone()).service(index)
-        // .service(create_user)
-        // .service(get_user)
-        // .service(update_user)
-        // .service(delete_user)
+        App::new()
+            .app_data(db_data.clone())
+            .configure(config::api_config::config_services)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
