600eecd0b64e6b9fa2b710c02841b22cf3da34da
Takashicc <105186894+Takashicc@users.noreply.github.com>
2022-12-30 20:43:14 +0900

    feat: Add user_exists_by_email, create_user

--- backend/src/models/user_model.rs
+++ backend/src/models/user_model.rs
@@ -1,9 +1,48 @@
-use mongodb::bson::oid::ObjectId;
+use mongodb::{
+    bson::{doc, oid::ObjectId},
+    results::InsertOneResult,
+};
 use serde::{Deserialize, Serialize};
+
+use crate::{errors::AppError, repository::mongodb_repos::MongoRepo};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct User {
     #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
     pub id: Option<ObjectId>,
     pub name: String,
+    pub email: String,
+    pub password: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct SignUpDTO {
+    pub name: String,
+    pub email: String,
+    pub password: String,
+}
+
+impl MongoRepo {
+    pub async fn user_exists_by_email(&self, email: &str) -> Result<bool, AppError> {
+        let filter = doc! {"email": email};
+        let user = self.user_col.find_one(filter, None).await?;
+
+        if user.is_none() {
+            return Ok(false);
+        }
+
+        Ok(true)
+    }
+
+    pub async fn create_user(&self, dto: SignUpDTO) -> Result<InsertOneResult, AppError> {
+        let user = User {
+            id: None,
+            name: dto.name,
+            email: dto.email,
+            password: dto.password,
+        };
+        let result = self.user_col.insert_one(user, None).await?;
+
+        Ok(result)
+    }
 }
