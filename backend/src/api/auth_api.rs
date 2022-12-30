ba71ea82be17607d0da7a5a0e80ce22cc796fa5d
Takashicc <105186894+Takashicc@users.noreply.github.com>
2022-12-30 20:43:55 +0900

    feat: Add auth_api

---
+++ backend/src/api/auth_api.rs
@@ -0,0 +1,34 @@
+use actix_web::{
+    post,
+    web::{Data, Json},
+    HttpResponse,
+};
+
+use crate::{
+    errors::{AppError, AppErrorType},
+    models::user_model::SignUpDTO,
+    repository::mongodb_repos::MongoRepo,
+};
+
+#[post("/signup")]
+pub async fn signup(db: Data<MongoRepo>, data: Json<SignUpDTO>) -> Result<HttpResponse, AppError> {
+    println!("Checking user exists by email");
+    let user_exists = db.user_exists_by_email(&data.email).await?;
+    if user_exists {
+        println!("User exists!!");
+        return Err(AppError {
+            error_type: AppErrorType::AlreadyExists,
+            status_code: None,
+            message: Some("User already exists".to_string()),
+        });
+    }
+
+    let hashed_password = bcrypt::hash(&data.password, bcrypt::DEFAULT_COST)?;
+    let dto = SignUpDTO {
+        name: data.name.to_owned(),
+        email: data.email.to_owned(),
+        password: hashed_password,
+    };
+    let result = db.create_user(dto).await?;
+    Ok(HttpResponse::Ok().json(result))
+}
