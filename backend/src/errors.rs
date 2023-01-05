7ef2e7cfc94703d842b6401bb286a4e7e6d8ced1
Takashicc <105186894+Takashicc@users.noreply.github.com>
2023-01-05 22:18:43 +0900

    fix, feat: Remove AlreadyExists enum, implement From trait to jsonwebtoken

--- backend/src/errors.rs
+++ backend/src/errors.rs
@@ -3,11 +3,13 @@ use core::fmt;
 use actix_web::{http::StatusCode, HttpResponse, ResponseError};
 use serde::Serialize;
 
+use crate::constants;
+
 #[derive(Debug)]
 pub enum AppErrorType {
     Db,
-    AlreadyExists,
     Server,
+    User,
 }
 
 #[derive(Debug)]
@@ -23,12 +25,40 @@ pub struct AppErrorResponse {
 }
 
 impl AppError {
-    pub fn message(&self) -> String {
+    pub fn new(
+        error_type: AppErrorType,
+        status_code: Option<StatusCode>,
+        message: Option<String>,
+    ) -> Self {
+        Self {
+            error_type,
+            status_code,
+            message,
+        }
+    }
+
+    pub fn user_error(message: &str) -> Self {
+        Self {
+            error_type: AppErrorType::User,
+            status_code: None,
+            message: Some(message.to_string()),
+        }
+    }
+
+    pub fn server_error(message: &str) -> Self {
+        Self {
+            error_type: AppErrorType::Server,
+            status_code: None,
+            message: Some(message.to_string()),
+        }
+    }
+
+    fn message(&self) -> String {
         if let Some(message) = &self.message {
             return message.to_owned();
         }
 
-        "An unexpected error has occurred".to_string()
+        constants::MESSAGE_UNEXPECTED_ERROR.to_string()
     }
 }
 
@@ -42,13 +72,18 @@ impl ResponseError for AppError {
     fn status_code(&self) -> StatusCode {
         match self.error_type {
             AppErrorType::Db => StatusCode::INTERNAL_SERVER_ERROR,
-            AppErrorType::AlreadyExists => StatusCode::BAD_REQUEST,
             AppErrorType::Server => StatusCode::INTERNAL_SERVER_ERROR,
+            AppErrorType::User => StatusCode::BAD_REQUEST,
         }
     }
 
     fn error_response(&self) -> HttpResponse<actix_web::body::BoxBody> {
-        HttpResponse::build(self.status_code()).json(AppErrorResponse {
+        let mut status_code = self.status_code();
+        if let Some(code) = self.status_code {
+            status_code = code;
+        }
+
+        HttpResponse::build(status_code).json(AppErrorResponse {
             message: self.message(),
         })
     }
@@ -68,6 +103,16 @@ impl From<mongodb::error::Error> for AppError {
 
 impl From<bcrypt::BcryptError> for AppError {
     fn from(_: bcrypt::BcryptError) -> Self {
+        Self {
+            error_type: AppErrorType::Server,
+            status_code: None,
+            message: None,
+        }
+    }
+}
+
+impl From<jsonwebtoken::errors::Error> for AppError {
+    fn from(_: jsonwebtoken::errors::Error) -> Self {
         Self {
             error_type: AppErrorType::Server,
             status_code: None,
