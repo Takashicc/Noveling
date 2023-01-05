ff3418890af71b572a4955bdc55f2b20cf9fc0ff
Takashicc <105186894+Takashicc@users.noreply.github.com>
2023-01-05 22:20:51 +0900

    feat: Add login route

--- backend/src/api/auth_api.rs
+++ backend/src/api/auth_api.rs
@@ -5,8 +5,10 @@ use actix_web::{
 };
 
 use crate::{
-    errors::{AppError, AppErrorType},
-    models::user_model::SignUpDTO,
+    auth::auth_token::{Claims, Token},
+    constants,
+    errors::AppError,
+    models::user_model::{LoginDTO, SignUpDTO},
     repository::mongodb_repos::MongoRepo,
 };
 
@@ -14,11 +16,7 @@ use crate::{
 pub async fn signup(db: Data<MongoRepo>, data: Json<SignUpDTO>) -> Result<HttpResponse, AppError> {
     let user_exists = db.user_exists_by_email(&data.email).await?;
     if user_exists {
-        return Err(AppError {
-            error_type: AppErrorType::AlreadyExists,
-            status_code: None,
-            message: Some("User already exists".to_string()),
-        });
+        return Err(AppError::user_error(constants::MESSAGE_USER_ALREADY_EXISTS));
     }
 
     let hashed_password = bcrypt::hash(&data.password, bcrypt::DEFAULT_COST)?;
@@ -29,4 +27,25 @@ pub async fn signup(db: Data<MongoRepo>, data: Json<SignUpDTO>) -> Result<HttpRe
     };
     let result = db.create_user(dto).await?;
     Ok(HttpResponse::Ok().json(result))
+}
+
+#[post("/login")]
+pub async fn login(db: Data<MongoRepo>, data: Json<LoginDTO>) -> Result<HttpResponse, AppError> {
+    let user = match db.find_user_by_email(&data.email).await? {
+        Some(v) => v,
+        None => {
+            return Err(AppError::user_error(
+                constants::MESSAGE_EMAIL_PASSWORD_INCORRECT,
+            ));
+        }
+    };
+
+    let is_valid_password = bcrypt::verify(&data.password, &user.password)?;
+    if !is_valid_password {
+        return Err(AppError::user_error(
+            constants::MESSAGE_EMAIL_PASSWORD_INCORRECT,
+        ));
+    }
+
+    Ok(HttpResponse::Ok().json(Token::new_bearer(Claims::generate_token(user.id)?)))
 }
--- backend/src/config/api_config.rs
+++ backend/src/config/api_config.rs
@@ -3,5 +3,11 @@ use actix_web::web::{self, ServiceConfig};
 use crate::api::auth_api;
 
 pub fn config_services(config: &mut ServiceConfig) {
-    config.service(web::scope("/api").service(web::scope("/auth").service(auth_api::signup)));
+    config.service(
+        web::scope("/api").service(
+            web::scope("/auth")
+                .service(auth_api::signup)
+                .service(auth_api::login),
+        ),
+    );
 }
