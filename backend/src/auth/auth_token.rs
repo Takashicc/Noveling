75c4cf2fe5244fba5e584d2d01394b4888ed4427
Takashicc <105186894+Takashicc@users.noreply.github.com>
2023-01-05 22:19:36 +0900

    feat: Add generate_token

---
+++ backend/src/auth/auth_token.rs
@@ -0,0 +1,51 @@
+use chrono::{Duration, Utc};
+use jsonwebtoken::{encode, EncodingKey, Header};
+use mongodb::bson::oid::ObjectId;
+use serde::{Deserialize, Serialize};
+
+use crate::{constants, errors::AppError};
+
+#[derive(Serialize, Deserialize)]
+pub struct Claims {
+    iat: i64,
+    exp: i64,
+    user_id: String,
+}
+
+impl Claims {
+    pub fn generate_token(id: Option<ObjectId>) -> Result<String, AppError> {
+        let user_id = match id {
+            Some(v) => v.to_string(),
+            None => return Err(AppError::server_error(constants::MESSAGE_UNEXPECTED_ERROR)),
+        };
+
+        let now = Utc::now();
+        let iat = now.timestamp();
+        let exp = (now + Duration::days(7)).timestamp();
+
+        Ok(encode(
+            &Header::default(),
+            &Self { iat, exp, user_id },
+            &EncodingKey::from_secret(constants::CONFIG.jwt_secret.as_ref()),
+        )?)
+    }
+}
+
+#[derive(Serialize)]
+pub enum TokenType {
+    Bearer,
+}
+
+#[derive(Serialize)]
+pub struct Token {
+    pub token: String,
+    pub token_type: TokenType,
+}
+impl Token {
+    pub fn new_bearer(token: String) -> Self {
+        Self {
+            token,
+            token_type: TokenType::Bearer,
+        }
+    }
+}
