2a1d09d44b6d0bde1e36c4d566aaa976e1d281b8
Takashicc <105186894+Takashicc@users.noreply.github.com>
2023-01-07 01:00:50 +0900

    feat: Add user_exists_by_id

--- backend/src/models/user_model.rs
+++ backend/src/models/user_model.rs
@@ -40,6 +40,20 @@ impl MongoRepo {
         Ok(true)
     }
 
+    pub async fn user_exists_by_id(&self, id: &str) -> Result<bool, AppError> {
+        let id = ObjectId::parse_str(id)?;
+        let filter = doc! {
+            "_id": id,
+        };
+        let user = self.user_col.find_one(filter, None).await?;
+
+        if user.is_none() {
+            return Ok(false);
+        }
+
+        Ok(true)
+    }
+
     pub async fn find_user_by_email(&self, email: &str) -> Result<Option<User>, AppError> {
         let filter = doc! {"email": email};
         let user = self.user_col.find_one(filter, None).await?;
