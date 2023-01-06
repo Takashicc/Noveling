8f2b0bd1df89eee1d7ded118b831b23ca9c114b9
Takashicc <105186894+Takashicc@users.noreply.github.com>
2023-01-07 01:02:15 +0900

    feat: Check user exists using token user_id

--- backend/src/auth/middleware.rs
+++ backend/src/auth/middleware.rs
@@ -1,7 +1,9 @@
-use actix_web::FromRequest;
-use futures::future::{err, ok, Ready};
+use std::pin::Pin;
 
-use crate::{constants, errors::AppError};
+use actix_web::{web::Data, FromRequest};
+use futures::Future;
+
+use crate::{constants, errors::AppError, repository::mongodb_repos::MongoRepo};
 
 use super::auth_token::Claims;
 
@@ -11,53 +13,63 @@ pub struct AuthService {
 
 impl FromRequest for AuthService {
     type Error = AppError;
-    type Future = Ready<Result<Self, AppError>>;
+    type Future = Pin<Box<dyn Future<Output = Result<Self, AppError>>>>;
 
     fn from_request(
         req: &actix_web::HttpRequest,
         _payload: &mut actix_web::dev::Payload,
     ) -> Self::Future {
-        let auth_header = req.headers().get("Authorization");
-        let token = match auth_header {
-            Some(v) => match v.to_str() {
-                Ok(v) => v,
-                Err(_) => return err(AppError::unexpected_error()),
-            },
-            None => {
-                return err(AppError::unauthorized_error(
-                    constants::MESSAGE_AUTHORIZATION_HEADER_MISSING,
-                ))
-            }
-        };
+        let req = req.clone();
 
-        let mut split_token = token.split_whitespace();
-        match split_token.next() {
-            Some(token_type) => {
-                if token_type != "Bearer" {
-                    return err(AppError::unauthorized_error(
-                        constants::MESSAGE_INVALID_TOKEN_TYPE,
-                    ));
+        Box::pin(async move {
+            let auth_header = req.headers().get("Authorization");
+            let token = match auth_header {
+                Some(v) => match v.to_str() {
+                    Ok(v) => v,
+                    Err(_) => return Err(AppError::unexpected_error()),
+                },
+                None => {
+                    return Err(AppError::unauthorized_error(
+                        constants::MESSAGE_AUTHORIZATION_HEADER_MISSING,
+                    ))
                 }
-            }
-            None => {
-                return err(AppError::unauthorized_error(
-                    constants::MESSAGE_TOKEN_TYPE_MISSING,
-                ))
-            }
-        };
+            };
 
-        let claims = match split_token.next() {
-            Some(token) => match Claims::validate_token(token) {
-                Ok(v) => v,
-                Err(e) => return err(e),
-            },
-            None => {
-                return err(AppError::unauthorized_error(
-                    constants::MESSAGE_TOKEN_MISSING,
-                ))
-            }
-        };
+            let mut split_token = token.split_whitespace();
+            match split_token.next() {
+                Some(token_type) => {
+                    if token_type != "Bearer" {
+                        return Err(AppError::unauthorized_error(
+                            constants::MESSAGE_INVALID_TOKEN_TYPE,
+                        ));
+                    }
+                }
+                None => {
+                    return Err(AppError::unauthorized_error(
+                        constants::MESSAGE_TOKEN_TYPE_MISSING,
+                    ))
+                }
+            };
 
-        ok(Self { claims })
+            let claims = match split_token.next() {
+                Some(token) => {
+                    let db = match req.app_data::<Data<MongoRepo>>() {
+                        Some(v) => v,
+                        None => return Err(AppError::unexpected_error()),
+                    };
+                    match Claims::validate_token(token, db).await {
+                        Ok(v) => v,
+                        Err(e) => return Err(e),
+                    }
+                }
+                None => {
+                    return Err(AppError::unauthorized_error(
+                        constants::MESSAGE_TOKEN_MISSING,
+                    ))
+                }
+            };
+
+            Ok(Self { claims })
+        })
     }
 }
