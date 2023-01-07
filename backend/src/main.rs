815636d9d64d0afe62a08316d463c04d119bf69f
Takashicc <105186894+Takashicc@users.noreply.github.com>
2023-01-07 17:03:17 +0900

    chore: Register cors middleware

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -6,7 +6,12 @@ mod errors;
 mod models;
 mod repository;
 
-use actix_web::{web::Data, App, HttpServer};
+use actix_cors::Cors;
+use actix_web::{
+    http::{header, Method},
+    web::Data,
+    App, HttpServer,
+};
 use repository::mongodb_repos::MongoRepo;
 
 #[actix_web::main]
@@ -17,6 +22,14 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(move || {
         App::new()
             .app_data(db_data.clone())
+            .wrap(
+                Cors::default()
+                    .allowed_origin("http://localhost:3000")
+                    .allowed_origin("http://127.0.0.1:3000")
+                    .allowed_methods(vec![Method::GET, Method::POST, Method::PUT, Method::DELETE])
+                    .allowed_headers(vec![header::AUTHORIZATION, header::CONTENT_TYPE])
+                    .max_age(3600),
+            )
             .configure(config::api_config::config_services)
     })
     .bind((constants::CONFIG.host.as_ref(), constants::CONFIG.port))?
