a5257c6c3a5b39c5cdb0dce34497da18bbf5e0ee
Takashicc <105186894+Takashicc@users.noreply.github.com>
2023-01-07 16:04:34 +0900

    feat: Add ping route

--- backend/src/config/api_config.rs
+++ backend/src/config/api_config.rs
@@ -1,14 +1,16 @@
 use actix_web::web::{self, ServiceConfig};
 
-use crate::api::auth_api;
+use crate::api::{auth_api, ping_api};
 
 pub fn config_services(config: &mut ServiceConfig) {
     config.service(
-        web::scope("/api").service(
-            web::scope("/auth")
-                .service(auth_api::signup)
-                .service(auth_api::login)
-                .service(auth_api::protected),
-        ),
+        web::scope("/api")
+            .service(
+                web::scope("/auth")
+                    .service(auth_api::signup)
+                    .service(auth_api::login)
+                    .service(auth_api::protected),
+            )
+            .service(ping_api::ping),
     );
 }
